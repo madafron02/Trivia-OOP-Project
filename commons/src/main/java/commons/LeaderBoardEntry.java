d110a2c01bb1e43e265680ba7a335640a129112f
Oana Madalina Fron <o.m.fron-1@student.tudelft.nl>
2022-03-05 18:19:36 +0100

    Solve pipeline fail
    Include one more class (GeneratedValue)

--- commons/src/main/java/commons/LeaderBoardEntry.java
+++ commons/src/main/java/commons/LeaderBoardEntry.java
@@ -4,10 +4,7 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 import java.io.Serializable;
 
