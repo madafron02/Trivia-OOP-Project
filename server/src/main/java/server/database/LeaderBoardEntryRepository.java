ea9fb899298a7cfbf267a160b4e3bb4bb1bf3403
Atanas Kichukov <A.B.Kichukov@student.tudelft.nl>
2022-03-07 11:50:22 +0100

    removed the LeaderboardEntry class controller and repository

--- server/src/main/java/server/database/LeaderBoardEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package server.database;
-
-import commons.LeaderBoardEntry;
-import commons.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeaderBoardEntryRepository extends JpaRepository<LeaderBoardEntry, Player> {
-}
