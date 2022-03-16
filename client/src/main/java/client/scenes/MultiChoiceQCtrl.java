b4bb8fc14804c75e4d2dec44e10c077a9ec05259
Atanas Kichukov <A.B.Kichukov@student.tudelft.nl>
2022-03-16 20:56:10 +0100

    made IDs and variables for text fields so its easier to use

--- client/src/main/java/client/scenes/MultiChoiceQCtrl.java
+++ client/src/main/java/client/scenes/MultiChoiceQCtrl.java
@@ -1,4 +1,15 @@
 package client.scenes;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class MultiChoiceQCtrl {
+    @FXML
+    private TextField answerA;
+    @FXML
+    private TextField answerB;
+    @FXML
+    private TextField answerC;
+    @FXML
+    private TextField question;
 }
--- client/src/main/java/client/scenes/NameSelectCtrl.java
+++ client/src/main/java/client/scenes/NameSelectCtrl.java
@@ -1,4 +1,9 @@
 package client.scenes;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class NameSelectCtrl {
+    @FXML
+    private TextField nameInput;
 }
