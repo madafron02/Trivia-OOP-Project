43a06f8c4d32560d6a4005e579cf2f3242e447af
Atanas Kichukov <A.B.Kichukov@student.tudelft.nl>
2022-03-17 12:24:46 +0100

    final pipeline fix

--- server/src/main/java/server/database/ActivityRepository.java
+++ server/src/main/java/server/database/ActivityRepository.java
@@ -1,7 +1,6 @@
 package server.database;
 
 import commons.Activity;
-import commons.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {}
