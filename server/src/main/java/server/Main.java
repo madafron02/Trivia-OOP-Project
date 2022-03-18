fe5f7d7aee6498b53e31925893c994f4fe690ed8
Robert Boboc <r.a.boboc-1@student.tudelft.nl>
2022-03-18 18:48:11 +0100

    Update server/src/main/java/server/Main.java

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -19,7 +19,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 
 @SpringBootApplication
