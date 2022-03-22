8eae0e06715e38690aa6e083da49e78d5ae06913
Khalit Gulamov <K.Gulamov@student.tudelft.nl>
2022-03-22 08:26:47 +0100

    Create a game class

---
+++ commons/src/main/java/commons/Game.java
@@ -0,0 +1,66 @@
+package commons;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    private List<Round> rounds;
+    private List<Player> players;
+
+    public Game() {
+    }
+
+    public Game(List<Round> rounds, List<Player> players) {
+        this.rounds = rounds;
+        this.players = players;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public List<Round> getRounds() {
+        return rounds;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public void setRounds(List<Round> rounds) {
+        this.rounds = rounds;
+    }
+
+    public String toString() {
+        String answer = "Game id: " + this.id + "\nPlayers:";
+        for (Player player : players) {
+            answer += player.getName() + "\n";
+        }
+        return answer;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+
+}
