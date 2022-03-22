56b2eb62494be3a9e4481cf2771b9ba4c510c13f
Khalit Gulamov <K.Gulamov@student.tudelft.nl>
2022-03-23 00:20:36 +0100

    Add all-time leaderboard functionality

--- client/src/main/java/client/scenes/AllTimeLeaderboardCtrl.java
+++ client/src/main/java/client/scenes/AllTimeLeaderboardCtrl.java
@@ -2,15 +2,32 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import commons.Player;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import javax.inject.Inject;
 import javafx.scene.control.Button;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
 
 public class AllTimeLeaderboardCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+
+    @FXML
+    private Button backFromLeaderboard;
+    @FXML
+    private TableView<Player> tableView = new TableView<>();
+    @FXML
+    private TableColumn<Player, String> playerName = new TableColumn<>();
+    @FXML
+    private TableColumn<Player, Integer> score = new TableColumn<>();
+    @FXML
+    private TableColumn<Player, String> place = new TableColumn<>();
 
     @Inject
     public AllTimeLeaderboardCtrl(ServerUtils server, MainCtrl mainCtrl) {
@@ -18,10 +35,15 @@ public class AllTimeLeaderboardCtrl {
         this.server = server;
     }
 
-    @FXML
-    private Button backFromLeaderboard;
-    @FXML
-    private TableView<Player> tableView;
+    public void initialize() {
+        List<Player> players = this.server.getPlayers();
+        players.sort((o1, o2) -> o2.getPoints() - o1.getPoints());
+        playerName.setCellValueFactory(new PropertyValueFactory<Player, String>("name"));
+        score.setCellValueFactory(new PropertyValueFactory<Player, Integer>("points"));
+        ObservableList<Player> observableList = FXCollections.observableList(players);
+        tableView.setItems(observableList);
+    }
+
 
     public void goBack() {
         mainCtrl.showSplash();
