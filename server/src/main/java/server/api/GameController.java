17d7befeff4e8cf07d9a00adc96710e17f62b247
Khalit Gulamov <K.Gulamov@student.tudelft.nl>
2022-03-23 17:52:01 +0100

    Creating a game class for the future multiplayer and the endpoints for it

---
+++ server/src/main/java/server/api/GameController.java
@@ -0,0 +1,40 @@
+package server.api;
+
+import commons.Game;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.database.GameRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/games")
+public class GameController {
+    GameRepository repo;
+
+    public GameController(GameRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping(path = {"", "/"})
+    public List<Game> getAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> getGame(@PathVariable long id){
+        if (id < 0 || !repo.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(repo.findById(id).get());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Game> addGame(@RequestBody Game game){
+        if(game.getId() < 0){
+            return ResponseEntity.badRequest().build();
+        }
+        Game saved = repo.save(game);
+        return ResponseEntity.ok(saved);
+    }
+}
