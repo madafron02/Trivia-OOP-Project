fe4d88a7337b0bef8fab4ac5693b0321471ff870
Khalit Gulamov <K.Gulamov@student.tudelft.nl>
2022-03-23 11:58:57 +0100

    Add JavaDoc to the all time leaderboard class

--- client/src/main/java/client/scenes/AllTimeLeaderboardCtrl.java
+++ client/src/main/java/client/scenes/AllTimeLeaderboardCtrl.java
@@ -33,12 +33,24 @@ public class AllTimeLeaderboardCtrl {
     @FXML
     private TableColumn<Player, Integer> place = new TableColumn<>();
 
+    /**
+     * Constructor
+     * @param server - serverUtils object with methods to add/get players to/from the server
+     * @param mainCtrl - main control class injection
+     */
     @Inject
     public AllTimeLeaderboardCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
         this.server = server;
     }
 
+    /**
+     * This method is automatically invoked when the scene is loaded.
+     * This method gets the players from the server, sorts them and fill the tableView columns
+     * using corresponding methods from javaFx.
+     * setCellValueFactory uses the getters from the Player class to automatically fill in all
+     * of the columns after passing the list of players to the tableView.
+     */
     public void initialize() {
         List<Player> players = this.server.getPlayers();
         players.sort((o1, o2) -> o2.getPoints() - o1.getPoints());
@@ -46,13 +58,14 @@ public class AllTimeLeaderboardCtrl {
             player.setPlace(players.indexOf(player) + 1);
         }
         playerName.setCellValueFactory(new PropertyValueFactory<Player, String>("name"));
-        score.setCellValueFactory(new PropertyValueFactory<Player, Integer>("points"));
         place.setCellValueFactory(new PropertyValueFactory<Player, Integer>("place"));
         ObservableList<Player> observableList = FXCollections.observableList(players);
         tableView.setItems(observableList);
     }
 
-
+    /**
+     * Go back button
+     */
     public void goBack() {
         mainCtrl.showSplash();
     }
