13762fd598228580fdd9b9d48854c140e7f32fd6
Renyi Yang <R.Yang-7@student.tudelft.nl>
2022-03-24 17:05:38 +0100

    add javadoc

--- server/src/test/java/server/api/ActivityTestRepository.java
+++ server/src/test/java/server/api/ActivityTestRepository.java
@@ -4,6 +4,11 @@ import commons.Activity;
 import server.database.ActivityRepository;
 
 public class ActivityTestRepository extends TestRepository<Activity> implements ActivityRepository {
+    /**
+     * find an activity according to its name(id field in the json file)
+     * @param activityName the name of the activity
+     * @return the matching activity
+     */
     public Activity findByActivityName(String activityName) {
         for(Activity activity: repo.values()){
             if(activity.getActivityName() == activityName)return activity;
