0c9959818e4a6cfd41e58e74830901bab975c3e0
Renyi Yang <R.Yang-7@student.tudelft.nl>
2022-03-24 13:55:34 +0100

    test one class

--- server/src/test/java/server/api/ActivityTestRepository.java
+++ server/src/test/java/server/api/ActivityTestRepository.java
@@ -5,6 +5,9 @@ import server.database.ActivityRepository;
 
 public class ActivityTestRepository extends TestRepository<Activity> implements ActivityRepository {
     public Activity findByActivityName(String activityName) {
+        for(Activity activity: repo.values()){
+            if(activity.getActivityName() == activityName)return activity;
+        }
         return null;
     }
 }
