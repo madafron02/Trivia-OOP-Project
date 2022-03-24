d8b686ac214218eb222337e38627c6ccc4b6fd2b
Renyi Yang <R.Yang-7@student.tudelft.nl>
2022-03-25 00:34:11 +0100

    test

---
+++ server/src/test/java/server/api/RoundControllerTest.java
@@ -0,0 +1,55 @@
+package server.api;
+
+import commons.Player;
+import commons.Question;
+import commons.Round;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoundControllerTest {
+    private RoundController sut;
+    private Round test;
+    @BeforeEach
+    public void init(){
+        RoundTestRepository repo = new RoundTestRepository();
+        List<String> answers = new ArrayList<>();
+        answers.add("a1");
+        answers.add("a2");
+        answers.add("a3");
+        Question q = new Question("q1",answers);
+        List<Player>p = new ArrayList<>();
+        p.add(new Player("p1"));
+        test = new Round(q,p);
+        repo.save(test);
+        sut = new RoundController(repo);
+    }
+    @Test
+    void getAll() {
+        List<Round>r = new ArrayList<>();
+        r.add(test);
+        assertEquals(r,sut.getAll());
+    }
+
+    @Test
+    void getRoundById() {
+        assertEquals(test,sut.getRoundById(test.getId()));
+    }
+
+    @Test
+    void addRound() {
+        List<String> answers = new ArrayList<>();
+        answers.add("b1");
+        answers.add("b2");
+        answers.add("b3");
+        Question q = new Question("q2",answers);
+        List<Player>p = new ArrayList<>();
+        p.add(new Player("p1"));
+        Round test2 = new Round(q,p);
+        assertEquals("The round has been saved",sut.addRound(test2).getBody());
+    }
+}
